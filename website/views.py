a4866f4539139dac848ee9c649b49f594b3ee5c9
henbarper <benjaminitonio@gmail.com>
2023-10-24 13:53:56 -0500

    adding notes

--- website/views.py
+++ website/views.py
@@ -1,15 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note = request.form.get('note')
 
-@views.route('/hello')
-def hello_world():
-   return "<h1>hello world</h1>"
+        if len(note) < 1:
+            flash('Note is too short', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
 
-@views.route('/bye')
-def bye_world():
-   return "<h1>goodbye world</h1>"
+    return render_template("home.html", user=current_user)
+
+# @views.route('/hello')
+# def hello_world():
+#    return "<h1>hello world</h1>"
+
+# @views.route('/bye')
+# def bye_world():
+#    return "<h1>goodbye world</h1>"
